ba673220f059751b7b5e964c56652dd4e4920ed1
Thibault Le Goff <44211215+Xorko@users.noreply.github.com>
2021-01-05 11:15:21 +0100

    Remove unused import

--- src/main/java/org/libmanager/server/repository/BookRepository.java
+++ src/main/java/org/libmanager/server/repository/BookRepository.java
@@ -4,8 +4,6 @@ import org.libmanager.server.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.Optional;
-
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
 }
